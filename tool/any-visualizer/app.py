5bd7d53f3cf566824875f15b3a81488bcac77ee3
Gustavo Guerino <gustavoguerino@hotmail.com>
2017-02-13 21:31:01 -0300

    Alter files to deploy

--- tool/any-visualizer/app.py
+++ tool/any-visualizer/app.py
@@ -189,4 +189,5 @@ def runiuga():
 
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, port=8080)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', use_reloader=True, port=port)
